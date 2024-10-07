0ffb8cc1763854c15c4b132ea04dff4aad8c830b
ArtyomBakirov04 <artyombakirov04@outlook.com>
2024-10-07 10:35:42 +0500

    create project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from "./components/header/Header";
+import { Set } from './components/set/Set';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Set />     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import './Card.css'
+export function Card({front, back}) {
+    const [checkCard, setCheckCard] = useState(false);
+    
+    const handleClick = () => {
+        setCheckCard(!checkCard);
+    }
+    let className = `card ${checkCard ? 'card-check' : ''}`;
+    return(
+        <div 
+            className={className} 
+            onClick={handleClick} 
+            data-back={back}
+        >
+            {front}
+        </div>
+    );
+}
