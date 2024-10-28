6b4d8da2f4e8b36711fc1d333482d9918c356059
ArtyomBakirov04 <artyombakirov04@outlook.com>
2024-10-28 10:29:49 +0500

    add routes

--- src/components/btnSet/BtnSet.jsx
+++ src/components/btnSet/BtnSet.jsx
@@ -1,8 +1,11 @@
 import "./BtnSet.css";
- export function BtnSet(){
-    return (
-        <button>
-            название кнопки
-        </button>
+import { Link } from "react-router-dom";
+export function BtnSet({name, id}){
+return (
+<li className="list-set__item">
+    <Link className="list-set__btn" to={`/set/${id}`} state={{set: name}}>
+            {name}
+        </Link>
+       </li>
     )
  }
